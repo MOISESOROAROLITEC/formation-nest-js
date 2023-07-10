e1a22b5dea335469a950e64df1acdabc1302e53e
moisesoroarolitec <moisesoro@arolitec.com>
2023-07-10 09:48:51 +0000

    add timeout interceptor

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -3,12 +3,14 @@ import { APP_GUARD, APP_INTERCEPTOR } from "@nestjs/core";
 import { ApiKeyGuard } from "./guards/api-key/api-key.guard";
 import { ConfigModule } from "@nestjs/config";
 import { WrapResponseInterceptor } from "./interceptors/wrap-response/wrap-response.interceptor";
+import { TimeoutInterceptor } from "./interceptors/timeout/timeout.interceptor";
 
 @Module({
   imports: [ConfigModule],
   providers: [
     { provide: APP_GUARD, useClass: ApiKeyGuard },
     { provide: APP_INTERCEPTOR, useClass: WrapResponseInterceptor },
+    { provide: APP_INTERCEPTOR, useClass: TimeoutInterceptor },
   ],
 })
 export class CommonModule {}
--- src/common/decorators/public.decorators.ts
+++ src/common/decorators/public.decorators.ts
@@ -1,5 +1,9 @@
 import { SetMetadata } from "@nestjs/common";
 
 export const IS_PUBLIC_KEY = "isPublic";
+export const IS_USER_KEY = "isUser";
+export const IS_ADMIN_KEY = "isAdmin";
 
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
+export const User = () => SetMetadata(IS_USER_KEY, true);
+export const Admin = () => SetMetadata(IS_ADMIN_KEY, true);
